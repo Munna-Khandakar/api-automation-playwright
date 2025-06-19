1ac13a4ed0c89fdd74b679533ab610d3b297ce9a
Munna-Khandakar <munnashisad@gmail.com>
2025-06-19 11:32:58 +0600

    env added

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,10 @@
 import {defineConfig, devices} from '@playwright/test';
+import * as dotenv from "dotenv";
 import {TestConfig} from "@type/TestConfig";
+import {PROJECT} from "@type/enums/Project";
+import {USER} from "@type/enums/User";
+
+dotenv.config();
 
 export default defineConfig<TestConfig>({
     testDir: './src/tests',
@@ -18,30 +23,19 @@ export default defineConfig<TestConfig>({
         trace: 'on-first-retry',
     },
 
-    projects: [
-        {
-            name: 'ideas',
+    projects: Object.values(PROJECT).map(project => {
+        const projectName = project.toUpperCase();
+        return {
+            name: project,
             use: {
-                baseURL: "https://ideas.ideascale.me",
+                baseURL: process.env[`${projectName}_BASE_URL`],
                 api_tokens: {
-                    SUPERUSER: "#",
-                    MODERATOR: "#",
-                    ADMIN: "#",
+                    [USER.SUPERUSER]: process.env[`${projectName}_SUPERUSER_API_TOKEN`],
+                    [USER.MODERATOR]: process.env[`${projectName}_MODERATOR_API_TOKEN`],
+                    [USER.ADMIN]: process.env[`${projectName}_ADMIN_API_TOKEN`],
                 },
                 ...devices['Desktop Chrome']
             },
-        },
-        {
-            name: 'unit',
-            use: {
-                baseURL: "https://ideas.ideascale.me",
-                api_tokens: {
-                    SUPERUSER: "#",
-                    MODERATOR: "#",
-                    ADMIN: "#",
-                },
-                ...devices['Desktop Chrome']
-            },
-        },
-    ],
+        };
+    }),
 });
