6b7e4a21b5b45e113bc88244cb336ed64d2cf3f0
Munna-Khandakar <munnashisad@gmail.com>
2025-06-19 18:58:07 +0600

    idea test case added

--- src/tests/idea/idea.get.spec.ts
+++ src/tests/idea/idea.get.spec.ts
@@ -3,6 +3,82 @@ import {test} from "@fixture/request-fixture";
 import {ArrayAssertionUtil} from "@utils/ArrayAssertionUtil";
 
 test.describe("GET", () => {
+    test("Get idea submission form for campaign", async ({request, api_tokens}) => {
+        const campaignId = 188;
+        for (const [userType, token] of Object.entries(api_tokens)) {
+            await test.step(userType, async () => {
+                const apiResponse = await request.token(token).get(`/idea-form/campaigns/${campaignId}`);
+                expect(apiResponse.status()).toBe(200);
+                const response = await apiResponse.json();
+                const formFields = response.formFields;
+                ArrayAssertionUtil.expectNonEmptyArray(formFields);
+                ArrayAssertionUtil.expectArrayWithFieldValue(formFields, "key", "title");
+                ArrayAssertionUtil.expectArrayWithFieldValue(formFields, "key", "text");
+            })
+        }
+    });
+
+    test("Get contributors for idea form", async ({request, api_tokens}) => {
+        const campaignId = 188;
+        for (const [userType, token] of Object.entries(api_tokens)) {
+            await test.step(userType, async () => {
+                const apiResponse = await request.token(token).get(`/idea-form/campaigns/${campaignId}/contributors`);
+                expect(apiResponse.status()).toBe(200);
+                const response = await apiResponse.json();
+                const content = response.content;
+                ArrayAssertionUtil.expectNonEmptyArray(content);
+                ArrayAssertionUtil.expectArrayWithFieldValue(content, "email", "rob.hoehn@ideascale.com");
+                ArrayAssertionUtil.expectArrayWithFieldValue(content, "userName", "robhoehn");
+            })
+        }
+    });
+
+    test("Get on behalf of members for idea form", async ({request, api_tokens}) => {
+        const campaignId = 188;
+        for (const [userType, token] of Object.entries(api_tokens)) {
+            await test.step(userType, async () => {
+                const apiResponse = await request.token(token).get(`/idea-form/campaigns/${campaignId}/on-behalf-of-members`);
+                expect(apiResponse.status()).toBe(200);
+                const response = await apiResponse.json();
+                const content = response.content;
+                ArrayAssertionUtil.expectNonEmptyArray(content);
+                ArrayAssertionUtil.expectArrayWithFieldValue(content, "email", "rob.hoehn@ideascale.com");
+                ArrayAssertionUtil.expectArrayWithFieldValue(content, "userName", "robhoehn");
+            })
+        }
+    });
+
+    test("Get draft ideas", async ({request, api_tokens}) => {
+        for (const [userType, token] of Object.entries(api_tokens)) {
+            await test.step(userType, async () => {
+                const apiResponse = await request.token(token).get("/idea-form/drafts");
+                expect(apiResponse.status()).toBe(200);
+                const response = await apiResponse.json();
+                const data = response.data;
+                ArrayAssertionUtil.expectNonEmptyArray(data);
+                ArrayAssertionUtil.expectArrayWithFieldValue(data, "title", "This is a daft Idea");
+                ArrayAssertionUtil.expectArrayWithFieldValue(data, "submitterId", "17409");
+                ArrayAssertionUtil.expectArrayWithFieldValue(data, "canDelete", true);
+            })
+        }
+    });
+
+    test.only("Get draft idea form", async ({request, api_tokens}) => {
+        const ideaId = 49495;
+        for (const [userType, token] of Object.entries(api_tokens)) {
+            await test.step(userType, async () => {
+                const apiResponse = await request.token(token).get(`/idea-form/drafts/${ideaId}`);
+                expect(apiResponse.status()).toBe(200);
+                const response = await apiResponse.json();
+                const formFields = response.formFields;
+                ArrayAssertionUtil.expectNonEmptyArray(formFields);
+                ArrayAssertionUtil.expectArrayWithFieldValue(formFields, "key", "title");
+                ArrayAssertionUtil.expectArrayWithFieldValue(formFields, "value", "This is a daft Idea");
+            })
+        }
+    });
+
+
     test("Get idea link qualifiers", async ({request, api_tokens}) => {
         for (const [userType, token] of Object.entries(api_tokens)) {
             await test.step(userType, async () => {
